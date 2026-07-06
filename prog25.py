b046099f2f12507096c017e37031d33d809364f3
maria.npinheiro <contato.malupinheiro@gmail.com>
2026-07-06 20:51:54 -0300

    prog25.py

---
+++ prog25.py
@@ -0,0 +1,9 @@
+v1 = int(input("Digite o 1° valor: "))
+v2 = int(input("Digite o 2° valor: "))
+
+if v1 > v2:
+    print(f"O primeiro valor {v1} é o maior.")
+elif v2 > v1:
+    print(f"O segundo valor {v2} é o maior.")
+else:
+    print("Ambos os valores são iguais.")
